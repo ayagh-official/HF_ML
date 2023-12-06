3e09503419a58de648ec6d5defd5973f46db8ed5
ayagh-official <150711097+ayagh-official@users.noreply.github.com>
2023-12-06 15:14:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from typing import Union
-
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import numpy as np
 from tensorflow.keras.models import load_model
 
 app = FastAPI()
+
+# Allow any origin
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 model_path = "./trainedModel.h5"
 binary_model = load_model(model_path)
@@ -26,6 +35,9 @@ class Item(BaseModel):
     ca: int
     thal: int
 
+@app.get("/ping")
+async def ping():
+    return "Hello, I am alive"
 
 @app.post("/predict")
 def predict(item: Item):
@@ -38,14 +50,11 @@ def predict(item: Item):
     # Make prediction using the loaded model
     prediction = binary_model.predict(input_data)
 
-    # The output is a probability, you can convert it to a class (0 or 1) based on a threshold
+   # The output is a probability, you can convert it to a class (0 or 1) based on a threshold
     threshold = 0.5
-    predicted_class = 1 if prediction[0, 0] > threshold else 0
+    prediction_message = "Positive" if prediction[0, 0] > threshold else "Negative"
 
-    # Define output messages based on the predicted class
-    output_messages = {
-        0: "No Heart Failure predicted for the patient",
-        1: "Heart Failure may develope for the patient"
-    }
-
-    return {"prediction": predicted_class, "message": output_messages[predicted_class]}
+    return {"prediction": prediction_message}
+    
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port=8000)
